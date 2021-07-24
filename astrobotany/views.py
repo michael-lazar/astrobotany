3925e75734042d3bdfc1691514bf41070a9ab417
Michael Lazar <lazar.michael22@gmail.com>
2021-07-24 13:48:37 -0400

    Sort by least recently watered

--- astrobotany/views.py
+++ astrobotany/views.py
@@ -825,30 +825,51 @@ def garden_view(request):
 
 
 @app.auth_route("/app/visit/sort_health")
-def garden_view(request):
+def garden_sort_health_view(request):
+    now = datetime.now()
+
     healthy_plants = (
         Plant.all_active()
-        .filter(Plant.score > 0, Plant.watered_at >= datetime.now() - timedelta(days=1))
-        .order_by(Plant.score.desc())
+        .filter(Plant.score > 0, Plant.watered_at >= now - timedelta(days=1))
+        .order_by(Plant.watered_at)
     )
     dry_plants = (
         Plant.all_active()
-        .filter(Plant.score > 0, Plant.watered_at < datetime.now() - timedelta(days=1), Plant.watered_at >= datetime.now() - timedelta(days=3))
-        .order_by(Plant.score.desc())
+        .filter(
+            Plant.score > 0,
+            Plant.watered_at < now - timedelta(days=1),
+            Plant.watered_at >= now - timedelta(days=3),
+        )
+        .order_by(Plant.watered_at)
     )
     wilting_plants = (
         Plant.all_active()
-        .filter(Plant.score > 0, Plant.watered_at < datetime.now() - timedelta(days=3), Plant.watered_at >= datetime.now() - timedelta(days=5))
-        .order_by(Plant.score.desc())
+        .filter(
+            Plant.score > 0,
+            Plant.watered_at < now - timedelta(days=3),
+            Plant.watered_at >= now - timedelta(days=5),
+        )
+        .order_by(Plant.watered_at)
     )
     dead_plants = (
         Plant.all_active()
-        .filter(Plant.score > 0, Plant.watered_at >= datetime.now() - timedelta(days=8), Plant.watered_at < datetime.now() - timedelta(days=5))
-        .order_by(Plant.score.desc())
+        .filter(
+            Plant.score > 0,
+            Plant.watered_at >= now - timedelta(days=8),
+            Plant.watered_at < now - timedelta(days=5),
+        )
+        .order_by(Plant.watered_at)
     )
 
     garden_art = render_art("trees.psci", ansi_enabled=request.cert.ansi_enabled)
-    body = request.render_template("garden_by_health.gmi", healthy_plants=healthy_plants, dry_plants=dry_plants, wilting_plants=wilting_plants, dead_plants=dead_plants, garden_art=garden_art)
+    body = request.render_template(
+        "garden_by_health.gmi",
+        healthy_plants=healthy_plants,
+        dry_plants=dry_plants,
+        wilting_plants=wilting_plants,
+        dead_plants=dead_plants,
+        garden_art=garden_art,
+    )
     return Response(Status.SUCCESS, "text/gemini", body)
 
 
